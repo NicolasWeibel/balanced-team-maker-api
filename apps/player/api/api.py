d6bc1c3945ac83bf3b4faa5399f2e303c8ed91d4
NicolasWeibel <123994887+NicolasWeibel@users.noreply.github.com>
2024-01-13 04:23:08 -0300

    Delete unused import

--- apps/player/api/api.py
+++ apps/player/api/api.py
@@ -1,5 +1,4 @@
 from django.db import IntegrityError, transaction
-from django.core.exceptions import ValidationError
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
